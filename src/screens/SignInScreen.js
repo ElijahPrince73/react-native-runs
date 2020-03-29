0627d738494b015b4c60e38066a53d769a1199ca
Elijah Prince <elijahprince73@gmail.com>
2020-03-28 20:04:38 -0700

    Sign Up styles

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { Text, Input, Button } from 'react-native-elements'
 import Spacer from '../components/Spacer'
 
 const SignInScreen = ({ navigation: { navigate } }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
       <Spacer>
-        <Input label="Email" />
+        <Input 
+          label="Email" 
+          value={email} 
+          onChangeText={setEmail} 
+          autoCapitalize={false} 
+          autoCorrect={false} 
+        />
       </Spacer>
       <Spacer>
-        <Input label="Password" />
+        <Input 
+          label="Password" 
+          value={password} 
+          onChangeText={setPassword} 
+          autoCapitalize={false} 
+          autoCorrect={false} 
+          secureTextEntry 
+        />
       </Spacer>
       <Spacer>
-        <Button title="Sign Up" onPress={() => navigate('SignUp')} />
+        <Button 
+          title="Sign Up" 
+          onPress={() => navigate('SignUp')} 
+        />
       </Spacer>
     </View>
   )
