93ff5ddd2956da8bc037ba4cf767408aaae3ed8d
Elijah Prince <elijahprince73@gmail.com>
2020-04-04 18:10:55 -0700

    added line to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import {StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import MapView, { Polyline } from 'react-native-maps'
 
-const Map = () => (
+const Map = () => {
+  let points = []
+  for(let i = 0; i < 20; i++) {
+    points.push({
+        latitude: 37.33233 - i * 0.002,
+        longitude: -122.03121 - i * 0.002
+    })
+  }
+
+  return (
   <MapView 
     style={styles.map} 
     initialRegion={{
@@ -11,8 +20,11 @@ const Map = () => (
       latitudeDelta: 0.01,
       longitudeDelta: 0.01
     }}
-  />
+  >
+    <Polyline coordinates={points} />
+  </MapView>
 )
+}
 
 const styles = StyleSheet.create({
   map: {
